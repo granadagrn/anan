3acb0b23f756e4482979cdc4238b11e98a99760a
granadagrn <sedaclbi@gmail.com>
2020-12-04 19:44:37 +0300

    cinnet1

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java World");
+        System.out.print("I am Seda Celebi");
+        System.out.print(" or may be Gundogdu");
+        System.out.println();
+        System.out.println("I am not sure about myself now");
+    }
+}
